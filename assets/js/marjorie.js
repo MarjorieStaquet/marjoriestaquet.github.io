a009406cad0d9d8cd19e5efaab636e04a82c6641
Marjorie STAQUET <staquet.marjorie@gmail.com>
2020-08-19 00:33:21 +0200

    Profile picture is ok

---
+++ assets/js/marjorie.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function () {
+
+
+
+/*-------------------------
+        TYPED JS
+-------------------------*/
+function typedJS() {
+
+    "use strict";
+
+    var $element = $(".element");
+    if($element.length){
+        var options = {
+            strings: $element.attr('data-elements').split(','),
+            typeSpeed: 100,
+            backDelay: 3000,
+            backSpeed: 50,
+            loop: true
+        };
+        var typed = new Typed(".element", options);
+    }
+}
+
+});
